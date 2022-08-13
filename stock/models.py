5ce185a258bedc2c9614431a32928067e8ce6f90
bobshort4bobby4 <treborken@gmail.com>
2022-08-13 21:30:00 +0000

    ADD available_sale field to vehicle model

--- stock/models.py
+++ stock/models.py
@@ -62,6 +62,7 @@ class Vehicle(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     odometer = models.IntegerField()
     colour = models.CharField(max_length=25)
+    available_sale = models.BooleanField(null=False, blank=False, default=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
