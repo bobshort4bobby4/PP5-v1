93da4620013916b61d6f8da643c1fd382f7666d7
bobshort4bobby4 <treborken@gmail.com>
2022-09-21 23:01:44 +0000

    removed disabled whsecret

--- pp5_carsales/settings.py
+++ pp5_carsales/settings.py
@@ -233,6 +233,6 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 # STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-STRIPE_WH_SECRET = "whsec_UeqPwrq6tL18JybnDPCLVeHnfXnNsmcn"
+
 
 
