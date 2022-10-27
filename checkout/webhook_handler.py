2303ee356464af0135f87e2ae0770925594b9e5a
bobshort4bobby4 <treborken@gmail.com>
2022-10-27 07:42:07 +0000

    ADD critical css in head

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -74,12 +74,13 @@ class StripeWH_Handler:
             else:
                 flag_info = True
             if flag_info:
+                shipdetail = shipping_details
                 profile.default_phone_number = shipping_details.phone
                 profile.default_country = shipping_details.address.country
                 profile.default_postcode = shipping_details.address.postal_code
                 profile.default_town_or_city = shipping_details.address.city
-                profile.default_street_address1 = shipping_details.address.line1 # !pep8
-                profile.default_street_address2 = shipping_details.address.line2 # !pep8
+                profile.default_street_address1 = shipdetail.address.line1
+                profile.default_street_address2 = shipdetail.address.line2
                 profile.default_county = shipping_details.address.state
                 profile.save()
 
@@ -165,7 +166,7 @@ class StripeWH_Handler:
                      'Created order in webhook'),
             status=200)
 
-    def handle_payment_intent_payment_failed(self, event):
+    def handle_payment_intent_failed(self, event):
         """
         Handle the payment_intent.payment_fail webhook from Stripe
         """
--- pp5_carsales/forms.py
+++ pp5_carsales/forms.py
@@ -1,13 +1,20 @@
+"""
+    Custom allauth form to clear session variables at login
+
+"""
+
 from allauth.account.forms import LoginForm
+
+
 class MyCustomLoginForm(LoginForm):
+    """ login form inheriting the allauth login
+        clears the session variables """
 
     def login(self, *args, **kwargs):
-
-        # Add your own processing here.
-        
+        """ over-ride the login method"""
         if "trade_flag" in self.request.session:
             self.request.session['trade_flag'] = {}
         if "bag" in self.request.session:
             del self.request.session["bag"]
-        # You must return the original result.
+
         return super(MyCustomLoginForm, self).login(*args, **kwargs)
